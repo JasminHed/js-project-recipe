a67e10424fe43a2a98e6ae63c8092c093a88abf4
Jasmin Hedlund <jasminhedlund@Jasmins-MacBook-Air.local>
2025-02-26 17:31:13 +0100

    updated id message in html and function in javascript

--- script.js
+++ script.js
@@ -1,8 +1,10 @@
+const messageBox = document.getElementById("message")
+
 const veganButton = document.getElementById("vegan-button")
 
 if (veganButton) {
   veganButton.addEventListener("click", () => {
-    console.log("veganButton was clicked")
+    messageBox.innerHTML = ("You chose vegan")
   })
 }
 
@@ -19,5 +21,19 @@ const nonvegButton = document.getElementById("nonveg-button")
 if (nonvegButton) {
   nonvegButton.addEventListener("click", () => {
     console.log("nonvegButton was clicked")
+  })
+}
+
+const descendingButton = document.getElementById("descending-button")
+if (descendingButton) {
+  descendingButton.addEventListener("click", () => {
+    console.log("descendingButton was clicked")
+  })
+}
+
+const ascendingButton = document.getElementById("ascending-button")
+if (ascendingButton) {
+  ascendingButton.addEventListener("click", () => {
+    console.log("ascendingButton was clicked")
   })
 }
