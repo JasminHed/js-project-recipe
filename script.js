21fa4cd53771019183cc4828604212a31b2a607a
Jasmin Hedlund <jasminhedlund@Jasmins-MacBook-Air.local>
2025-02-28 09:24:27 +0100

    updated comments in js, mediaqueri for tablet and removed a styling element

--- script.js
+++ script.js
@@ -1,41 +1,37 @@
-const messageHolder = document.getElementById("message") //getting element and saving in messageHolder
+//getting elements and saving to messageHolder
+const messageHolder = document.getElementById("message")
 const veganButton = document.getElementById("vegan-button")
 const descendingButton = document.getElementById("descending-button")
 
-//creating function called getRecipe w. parameter diet. The parameter returns recipe
+//functions getRecipe/handleSelection w. parameter diet/userChoice.
 const getRecipe = (diet) => {
   if (diet === "vegan") return "falafel"
 }
-
-//creating function called handleSelection w. parameter userChoice
 const handleSelection = (userChoice) => {
-  //creating it empty to fill later, like let winner from live session
-  let message = ""
+  let message = "" //creating it empty to fill specific message below, like let winner from live session
 
-  //if a user chooses vegan = message below
+  //if a user chooses vegan/descending = message below (ES6?)
   if (userChoice === "vegan") {
-    message = `You chose vegan? Here is a suited recipe ${getRecipe("vegan")}`
+    message = `You chose vegan? Hope you like ${getRecipe("vegan")}` //this actives my function above and its return
   }
   else if (userChoice === "descending") {
     message = "Aha, short on time? Not a problem!"
   }
 
-  //Message show in messageHolder
+  //Show message in messageHolder
   messageHolder.innerHTML += message
 }
 
-
-if (veganButton) { //Checking that button exists then veganButton clicked, call function handleSelection to send message.
+//Checking that button exists. When each button clicked, function above will send message.
+if (veganButton) {
   veganButton.addEventListener("click", () => {
     handleSelection("vegan")
   })
 }
-
 
 if (descendingButton) {
   descendingButton.addEventListener("click", () => {
     handleSelection("descending")
   })
 }
-
 
