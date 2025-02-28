9831f42574dd0ab97f66e98b464ea4f2b4686331
Jasmin Hedlund <jasminhedlund@Jasmins-MacBook-Air.local>
2025-02-28 11:57:41 +0100

    updated comments in js and put p in messages

--- script.js
+++ script.js
@@ -3,19 +3,19 @@ const messageHolder = document.getElementById("message")
 const veganButton = document.getElementById("vegan-button")
 const descendingButton = document.getElementById("descending-button")
 
-//functions getRecipe/handleSelection w. parameter diet/userChoice.
+//functions getRecipe/handleSelection w. parameter diet/userChoice. 
 const getRecipe = (diet) => {
   if (diet === "vegan") return "falafel"
 }
 const handleSelection = (userChoice) => {
   let message = "" //creating it empty to fill specific message below, like let winner from live session
 
-  //if a user chooses vegan/descending = message below (ES6?)
+  //cond.statements, if a user chooses vegan/descending = message below (ES6?)
   if (userChoice === "vegan") {
-    message = `You chose vegan? Hope you like ${getRecipe("vegan")}` //this actives my function above and its return
+    message = `<p>You chose vegan? Hope you like ${getRecipe("vegan")}</p>` //this way of writing it actives my function above and its return
   }
   else if (userChoice === "descending") {
-    message = "Aha, short on time? Not a problem!"
+    message = "<p>Aha, short on time? Not a problem!</p>" //a string because nothing else is needed
   }
 
   //Show message in messageHolder
@@ -34,7 +34,8 @@ if (descendingButton) {
     handleSelection("descending")
   })
 }
-//Or is this right?
+//This also gives messages. 
+
 /*const allButton = document.getElementById("all-button")
 if (allButton) {
   allButton.addEventListener("click", () => {
